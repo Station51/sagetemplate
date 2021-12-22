aa1160967404280aa471a7e035aa43fe16c78e32
pawelhop <paweldabrowa@hopsoftware.com>
2021-12-22 08:59:30 +0000

    Build an example of a block

--- app/admin.php
+++ app/admin.php
@@ -22,3 +22,11 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+/**
+ * Extend Gutenberg Blocks
+ * @link https://developer.wordpress.org/reference/hooks/enqueue_block_editor_assets/
+ */
+add_action('enqueue_block_editor_assets', function() {
+    wp_enqueue_script('sage/gutenberg.js', asset_path('scripts/gutenberg.js'), ['wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor'], null, true);
+} );
---
+++ resources/views/blocks/block-1.blade.php
@@ -0,0 +1,11 @@
+{{--
+  Title: Block-1 Nav
+  Category: common
+  Icon: awards
+--}}
+
+<section data-{{ $block['id'] }} id="{{ $block['id'] }}" class="{{ $block['classes'] }} section block-1-nav">
+  <div class="block-1-nav__wrapper">
+    <h1 class="block-1-nav__wrapper--title"></h1>
+  </div>
+</section>
