e1c66348be60f9aff277cca7c35d71f096a628de
pawelhop <paweldabrowa@hopsoftware.com>
2022-01-11 18:31:05 +0000

    Add Social Icons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,16 @@
 <footer class="content-info">
   <div class="container">
-    {{-- @php dynamic_sidebar('sidebar-footer') @endphp --}}
+    @if(get_field('company_logo_image', 'option')) 
+      <div class="content-info__logo">
+        <a class="content-info__logo--brand" href="{{ home_url('/') }}">
+          {{ get_field('company_logo_image', 'option') }}
+        </a>
+      </div>
+    @endif
+    {{-- @php dynamic_sidebar('sidebar-footers') @endphp --}}
+    <div class="content-info__social">
+       @include('partials/social')
+    </div>
   </div>
 </footer>
+
