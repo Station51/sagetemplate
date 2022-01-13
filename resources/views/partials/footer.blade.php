04dc7a0cba2fc4bee5c6b18709191b39cc3ffd26
pawelhop <paweldabrowa@hopsoftware.com>
2022-01-13 11:40:34 +0000

    Add contact fields

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,30 @@
-<footer class="content-info">
-  <div class="container">
-    @if(get_field('company_logo_image', 'option')) 
-      <div class="content-info__logo">
-        <a class="content-info__logo--brand" href="{{ home_url('/') }}">
-          {{ get_field('company_logo_image', 'option') }}
+<footer class="footer-info">
+  <div class="container footer-info__inner">
+    @if(get_field('logo', 'option')) 
+      <div class="footer-info__logo">
+        <a class="footer-info__logo--brand" href="{{ home_url('/') }}">
+          {!! wp_get_attachment_image(get_field('logo', 'option', 'thumbnail')) !!}
         </a>
       </div>
     @endif
-    {{-- @php dynamic_sidebar('sidebar-footers') @endphp --}}
-    <div class="content-info__social">
-       @include('partials/social')
+    @php dynamic_sidebar('sidebar-footer') @endphp
+    <div class="footer-info__social">
+      @include('partials/social')
+    </div>
+    <div class="footer-info__email">
+      email
+    </div>
+    <div class="footer-info__contact">
+      @include('partials/contact')
+    </div>
+    <div class="footer-info__email">
+    logo
+    </div>
+    <div class="footer-info__email">
+    logo
+    </div>
+    <div class="footer-info__email">
+    logo
     </div>
   </div>
 </footer>
