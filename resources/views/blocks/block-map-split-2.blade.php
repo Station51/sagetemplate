6014f79b0a6a33a2257f7c63c4e816ae3631fe88
waynehop <waynewilliams@hopsoftware.com>
2022-09-27 15:24:18 +0100

    update code to dry principle

--- resources/views/blocks/block-map-split-2.blade.php
+++ resources/views/blocks/block-map-split-2.blade.php
@@ -9,15 +9,14 @@
   $image = get_field('header_image');
   $size = 'large'; // (thumbnail, medium, large, full or custom size)
   $attachment_title = get_the_title($attach_id); 
+  $mapPosition = get_field('map_position');
 @endphp
 <section>
   {!! wp_get_attachment_image( $image, $size, $attachment_title ) !!}
 </section>
 <div class="container form-cont">
   <section data-{{ $block['id'] }} id="{{ $section_id ? $section_id : $block['id'] }}" class="{{ $block['classes'] }} section-bottom">
-    <article class="grid grid__outer">
-  
-      @if(get_field('map_position') == 'left') 
+    <article class="grid grid__outer {{ ($mapPosition) ? 'left' : 'right' }}">
   
         @if(get_field('google_map_iframe'))
           <div class="map-wrapper">
@@ -55,54 +54,11 @@
   
         @if(get_field('contact_form')) 
           <div class="form-wrapper">
-            <div class="form-block" data-aos="flip-left">
+            <div class="form-block" data-aos="flip-up">
               {!! get_field('contact_form') !!}
             </div>
           </div>
         @endif
-            
-      @else
-  
-        @if(get_field('contact_form')) 
-          <div class="form-wrapper">
-            <div class="form-block" data-aos="flip-right">
-              {!! get_field('contact_form') !!}
-            </div>
-          </div>
-        @endif
-  
-        @if(get_field('google_map_iframe'))
-          <div class="map-wrapper">
-            <div class="intro">
-              @if(get_field('form_title')) 
-                <h3>{!! get_field('form_title') !!}</h3>
-              @endif
-              @if(get_field('form_intro')) 
-                <p>{!! get_field('form_intro') !!}</p>
-              @endif
-            </div>
-            @if(get_field('telephone', 'option')) 
-              <div class="info-div">
-                <a href="tel:{{ get_field('telephone', 'option') }}">
-                  {!! get_field('telephone', 'option') !!}
-                </a>
-              </div>
-            @endif
-            @if(get_field('email', 'option')) 
-              <div class="info-div">
-                <a href="mailto:{{ get_field('email', 'option') }}" target="_blank">
-                  {!! get_field('email', 'option') !!}
-                </a>
-              </div>
-            @endif
-            <div class="info-div">Showroom:</div>
-            <div class="iframe-cont">
-              {!! get_field('google_map_iframe') !!}
-            </div>
-          </div>
-        @endif
-  
-      @endif
   
     </article>
   </section>
--- resources/views/blocks/block-slider-split-1.blade.php
+++ resources/views/blocks/block-slider-split-1.blade.php
@@ -10,95 +10,48 @@
   $buttonTwoLink = get_field('button_two_link');
   $section_bg_color = get_field('section_bg_color');
   $addSpace = get_field('section_layout_style');
+  $galleryPosition = get_field('gallery_position');
 @endphp
-@if(get_field('text_position') == 'left') 
 
-  <section data-{{ $block['id'] }} id="{{ $section_id ? $section_id : $block['id'] }}" class="{{ $block['classes'] }} {{ $section_bg_color }}">
+<section data-{{ $block['id'] }} id="{{ $section_id ? $section_id : $block['id'] }}" class="{{ $block['classes'] }} {{ $section_bg_color }} @if($galleryPosition) gallery-left @endif">
 
-    <div class="grid grid__outer {{ $addSpace }}">
+  <div class="grid grid__outer {{ $addSpace }}">
 
-      @if(get_field('static_content')) 
-        <article class="text-block left">
-          <div data-aos="fade-right" data-aos-duration="1000">
-            {!! get_field('static_content') !!}
-            @if (get_field('button_type') == 'popup')
+    @if(get_field('static_content')) 
+      <article class="text-block {{ ($galleryPosition) ? 'right' : 'left' }}">
+        <div data-aos="fade-up" data-aos-duration="1000">
+          {!! get_field('static_content') !!}
+          @if (get_field('button_type') == 'popup')
+            <div class="block-slider-split-1__btns">
+              @if(get_field('button_popup_text'))
+                <a href="" class="btn block-slider-split-1__btn {!! get_field('popup_class') !!}">{!! get_field('button_popup_text') !!}</a>
+              @endif
+            </div>
+          @else
+            @if($buttonOneLink || $buttonTwoLink)
               <div class="block-slider-split-1__btns">
-                @if(get_field('button_popup_text'))
-                  <a href="" class="btn block-slider-split-1__btn {!! get_field('popup_class') !!}">{!! get_field('button_popup_text') !!}</a>
+                @if($buttonOneLink)
+                  <a href="{!! $buttonOneLink['url'] !!}" target="{{ $buttonOneLink['target'] }}" class="btn block-slider-split-1__btn">{!! $buttonOneLink['title'] !!}</a>
+                @endif
+                @if($buttonTwoLink)
+                  <a href="{!! $buttonTwoLink['url'] !!}" target="{{ $buttonTwoLink['target'] }}" class="btn block-slider-split-1__btn">{!! $buttonTwoLink['title'] !!}</a>
                 @endif
               </div>
-            @else
-              @if($buttonOneLink || $buttonTwoLink)
-                <div class="block-slider-split-1__btns">
-                  @if($buttonOneLink)
-                    <a href="{!! $buttonOneLink['url'] !!}" target="{{ $buttonOneLink['target'] }}" class="btn block-slider-split-1__btn">{!! $buttonOneLink['title'] !!}</a>
-                  @endif
-                  @if($buttonTwoLink)
-                    <a href="{!! $buttonTwoLink['url'] !!}" target="{{ $buttonTwoLink['target'] }}" class="btn block-slider-split-1__btn">{!! $buttonTwoLink['title'] !!}</a>
-                  @endif
-                </div>
-              @endif
             @endif
-          </div>
-        </article>
-      @endif
+          @endif
+        </div>
+      </article>
+    @endif
 
-      @if(Blocks::getGallery())
-        <article data-{{ $block['id'] }} id="{{ $block['id'] }}" class="{{ $block['classes'] }} block-slider">
-            @foreach(Blocks::getGallery() as $image )
-              <div class="block-slider-split-1__column" style="background-image: url({!! $image !!})" role="img">
-                <div class="block-slider-split-1__banner"></div>
-              </div>
-            @endforeach
-        </article>
-      @endif
+    @if(Blocks::getGallery())
+      <article data-{{ $block['id'] }} id="{{ $block['id'] }}" class="{{ $block['classes'] }} block-slider">
+          @foreach(Blocks::getGallery() as $image )
+            <div class="block-slider-split-1__column" style="background-image: url({!! $image !!})" role="img">
+              <div class="block-slider-split-1__banner"></div>
+            </div>
+          @endforeach
+      </article>
+    @endif
 
-    </div>
-  </section>
-
-@else
-
-  <section data-{{ $block['id'] }} id="{{ $section_id ? $section_id : $block['id'] }}" class="{{ $block['classes'] }} {{ $section_bg_color }} section-flip">
-
-    <div class="grid grid__outer {{ $addSpace }}">
-
-      @if(Blocks::getGallery())
-        <article data-{{ $block['id'] }} id="{{ $block['id'] }}" class="{{ $block['classes'] }} block-slider">
-            @foreach(Blocks::getGallery() as $image )
-              <div class="block-slider-split-1__column" style="background-image: url({!! $image !!})" role="img">
-                <div class="block-slider-split-1__banner"></div>
-              </div>
-            @endforeach
-        </article>
-      @endif
-
-      @if(get_field('static_content')) 
-        <article class="text-block right">
-          <div data-aos="fade-left" data-aos-duration="1000">
-            {!! get_field('static_content') !!}
-            @if (get_field('button_type') == 'popup')
-              <div class="block-slider-split-1__btns">
-                @if(get_field('button_popup_text'))
-                  <a href="" class="btn block-slider-split-1__btn {!! get_field('popup_class') !!}">{!! get_field('button_popup_text') !!}</a>
-                @endif
-              </div>
-            @else
-              @if($buttonOneLink || $buttonTwoLink)
-                <div class="block-slider-split-1__btns">
-                  @if($buttonOneLink)
-                    <a href="{!! $buttonOneLink['url'] !!}" target="{{ $buttonOneLink['target'] }}" class="btn block-slider-split-1__btn">{!! $buttonOneLink['title'] !!}</a>
-                  @endif
-                  @if($buttonTwoLink)
-                    <a href="{!! $buttonTwoLink['url'] !!}" target="{{ $buttonTwoLink['target'] }}" class="btn block-slider-split-1__btn">{!! $buttonTwoLink['title'] !!}</a>
-                  @endif
-                </div>
-              @endif
-            @endif
-          </div>
-        </article>
-      @endif
-
-    </div>
-  </section>
-
-@endif
+  </div>
+</section>
